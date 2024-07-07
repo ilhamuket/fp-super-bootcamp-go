8885a4d80bfb0b67f56902ce20046cb83bb13d92
Muhammad Ilham <ilhamgallagher633@gmail.com>
2024-07-08 01:14:22 +0700

    fix: config for heroku

--- config/config.go
+++ config/config.go
@@ -2,21 +2,16 @@ package config
 
 import (
 	"final-project-golang-individu/models"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres" // import PostgreSQL driver
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // import PostgreSQL driver
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
 	// Read environment variables
 	dbUsername := os.Getenv("DB_USERNAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -26,20 +21,27 @@ func InitDB() {
 
 	// Connect to the database
 	var err error
-	DB, err = gorm.Open("postgres", "host="+dbHost+" port="+dbPort+" user="+dbUsername+" dbname="+dbName+" password="+dbPassword+" sslmode=disable")
+	DB, err = gorm.Open("postgres", "host="+dbHost+" port="+dbPort+" user="+dbUsername+" dbname="+dbName+" password="+dbPassword+" sslmode=require")
 	if err != nil {
 		log.Fatal("Failed to connect database", err)
 	}
 	DB.LogMode(true)
+
+	// Migrate database schema
 	migrate()
 }
 
 func migrate() {
+	// Auto migrate all models
 	DB.AutoMigrate(&models.User{}, &models.Role{}, &models.Profile{}, &models.News{}, &models.Comment{}, &models.UserRole{})
+
+	// Add foreign keys
 	DB.Model(&models.Profile{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
 	DB.Model(&models.News{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
 	DB.Model(&models.Comment{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
 	DB.Model(&models.Comment{}).AddForeignKey("news_id", "news(id)", "CASCADE", "CASCADE")
+
+	// Create default roles if they do not exist
 	createDefaultRoles()
 }
 
